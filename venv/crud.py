82cc59bd6a2b52eccdbb35025910c54dc2ecc6be
professoraamanda <amanda.xavier@pe.senac.br>
2024-06-03 12:42:03 -0300

    programadorDeSistema

---
+++ venv/crud.py
@@ -0,0 +1,68 @@
+import sqlite3
+
+def commit_close(funcao):
+    def decorator(*argumentos):
+        con = sqlite3.connect('base_DeDados.db')
+        cur = con.cursor() #conectando cursor
+        sql = funcao(*argumentos)
+        cur.execute(sql)
+        con.commit()
+        con.close()
+        print("ok!")
+    return decorator
+
+#nome = input("nome: ")
+#sobrenome = input("sobrenome: ")
+#cpf = input("cpf: ")
+#tempoDeServico = int(input("Tempo de serviço: "))
+#remuneracao = int(input("Remuneração: "))
+
+@commit_close
+def db_insert(nome, sobrenome, cpf, tempoDeServico, remuneracao):
+    return """
+    INSERT INTO funcionarios (nome, sobrenome, cpf, tempoDeServico, remuneracao)
+        VALUES('{}','{}','{}', {}, {})
+    """.format(nome, sobrenome, cpf, tempoDeServico, remuneracao)
+
+@commit_close
+def db_updateNome(nome, cpf):
+    return """
+    UPDATE funcionarios SET nome = '{}' 
+    WHERE cpf = '{}'  """.format(nome, cpf)
+
+@commit_close
+def db_updateSobrenome(sobrenome, cpf):
+    return """
+    UPDATE funcionarios SET sobrenome = '{}' 
+    WHERE cpf = '{}'  """.format(sobrenome, cpf)
+
+@commit_close
+def db_updateCPF(cpf, id):
+    return """
+    UPDATE funcionarios SET cpf = '{}' 
+    WHERE id = '{}'  """.format(cpf, id)
+
+@commit_close
+def db_updateTS(tempoDeServico, cpf):
+    return """
+    UPDATE funcionarios SET tempoDeServico = '{}' 
+    WHERE cpf = '{}'  """.format(tempoDeServico, cpf)
+
+@commit_close
+def db_updateRemuneracao(remuneracao, cpf):
+    return """
+    UPDATE funcionarios SET remuneracao = '{}' 
+    WHERE cpf = '{}'  """.format(remuneracao, cpf)
+
+def db_delete(cpf):
+    return """
+    DELETE FROM funcionarios 
+    WHERE cpf = '{}'  """.format(cpf)
+
+print("""
++-------------------------+
+ ****Deletar cadastro****
++-------------------------+
+""")
+cpf = int(input("informe o CPF:\n"))
+
--- venv/senac/atualizar.py
+++ venv/senac/atualizar.py
@@ -1,31 +1,4 @@
-import sqlite3
-con = sqlite3.connect('venv/senac/base_DeDados.db')
-cur = con.cursor() #conectando cursor
-
-def db_updateNome(nome, cpf):
-    return """
-    UPDATE funcionarios SET nome = '{}' 
-    WHERE cpf = '{}'  """.format(nome, cpf)
-
-def db_updateSobrenome(sobrenome, cpf):
-    return """
-    UPDATE funcionarios SET sobrenome = '{}' 
-    WHERE cpf = '{}'  """.format(sobrenome, cpf)
-
-def db_updateCPF(cpf, id):
-    return """
-    UPDATE funcionarios SET cpf = '{}' 
-    WHERE id = '{}'  """.format(cpf, id)
-
-def db_updateTS(tempoDeServico, cpf):
-    return """
-    UPDATE funcionarios SET tempoDeServico = '{}' 
-    WHERE cpf = '{}'  """.format(tempoDeServico, cpf)
-
-def db_updateRemuneracao(remuneracao, cpf):
-    return """
-    UPDATE funcionarios SET remuneracao = '{}' 
-    WHERE cpf = '{}'  """.format(remuneracao, cpf)
+from crud import db_updateNome, db_updateRemuneracao, db_updateCPF, db_updateSobrenome, db_updateTS
 
 print("""
 +-------------------------+
@@ -47,22 +20,19 @@ Qual campo será alterado?\n
 campo = int(input())
 if(campo==1):
     nome = input("novo nome: ")
-    cur.execute(db_updateNome(nome, cpfE))
+    db_updateNome(nome, cpfE)
 elif(campo==2):
     sobrenome = input("novo sobrenome: ")
-    cur.execute(db_updateSobrenome(sobrenome, cpfE))
+    db_updateSobrenome(sobrenome, cpfE)
 elif(campo==3):
     cpfNovo = input("novo cpf: ")
     id = int(input("Qual o id do usuário?\n"))
-    cur.execute(db_updateCPF(cpfNovo, id))
+    db_updateCPF(cpfNovo, id)
 elif(campo==4):
     tempoDeServico = input("novo tempo de serviço: ")
-    cur.execute(db_updateTS(tempoDeServico, cpfE))
+    db_updateTS(tempoDeServico, cpfE)
 elif(campo==5):
     remuneracao = input("nova remuneração: ")
-    cur.execute(db_updateRemuneracao(remuneracao, cpfE))
+    db_updateRemuneracao(remuneracao, cpfE)
 
 print("Atualização executada com sucesso!")
-
-con.commit() #executar os comandos na nossa base]
-con.close()
